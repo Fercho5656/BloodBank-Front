a6ec9380912cdca6c2c68a0ffd6f37c6dea355c9
Fercho5656 <morenomoranfernando@gmail.com>
2021-10-23 13:55:53 -0500

    New donation form

---
+++ src/components/NewDonation.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Registrar Nueva Donación</h1>
+  <form>
+    <div class="mb-3">
+      <label for="donationUser" class="form-label">Donador</label>
+      <input
+        type="text"
+        class="form-control"
+        id="donationUser"
+        placeholder="Fernando Moreno"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="donationQuantity" class="form-label">Cantidad (mL)</label>
+      <input
+        type="number"
+        min="0"
+        class="form-control"
+        id="donationQuantity"
+        placeholder="0 mL"
+      />
+    </div>
+    <div class="mb-3">
+      <button class="btn btn-success">Registrar Donación</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "NewDonation",
+};
+</script>
+
+<style>
+</style>
