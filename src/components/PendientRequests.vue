455c56757d875be1f5e7cd5736109fc8381097ec
Fercho5656 <morenomoranfernando@gmail.com>
2021-10-23 13:57:30 -0500

    Table to show requests to be checked

---
+++ src/components/PendientRequests.vue
@@ -0,0 +1,42 @@
+<template>
+  <h2>Peticiones Pendientes</h2>
+  <table class="table table-dark table-hover">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Fecha</th>
+        <th>Hospital</th>
+        <th>Grupo Sanguíneo</th>
+        <th>Cantidad (mL)</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>12/10/2021</td>
+        <td>Hospital San Fercho</td>
+        <td>A+</td>
+        <td>500mL</td>
+        <td class="buttons">
+          <button class="btn btn-success">Aceptar</button>
+          <button class="btn btn-primary">Rechazar</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+    name: 'PendientRequests'
+};
+</script>
+
+<style scoped>
+.buttons {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+}
+</style>
