baec741cf50d6fe32b4537361c3226b34ed1cdd2
Fercho5656 <morenomoranfernando@gmail.com>
2021-10-23 13:58:15 -0500

    Table to show requests history -To be changed for readonly table-

---
+++ src/components/RequestsHistory.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2>Historial de Peticiones</h2>
+  <table class="table table-dark table-hover">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Fecha</th>
+        <th>Hospital</th>
+        <th>Grupo Sanguíneo</th>
+        <th>Cantidad</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>5</td>
+        <td>11/10/2021</td>
+        <td>Hospital San Fercho</td>
+        <td>AB+</td>
+        <td>1500 mL</td>
+        <td class="text-warning"><strong>Rechazada</strong></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "RequestsHistory",
+};
+</script>
+
+<style>
+</style>
