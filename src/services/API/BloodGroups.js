3f976fcc56364b9638e89b5f1920f74f395550b0
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-11-02 14:49:36 -0600

    Method to get blood groups

--- src/services/API/BloodGroups.js
+++ src/services/API/BloodGroups.js
@@ -1,10 +1,21 @@
 const ENDPOINT = 'https://localhost:5001/api/bloodbank_bloodgroup';
 
-export const getAll = async () => {
+export const getBloodGroups = async () => {
     try {
         const response = await fetch(ENDPOINT);
         const data = await response.json();
         return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const getBloodGroupsInfo = async () => {
+    try {
+        const response = await fetch(ENDPOINT);
+        const { $values } = await response.json();
+        const bloodGroups = $values.map(data => data.bloodGroup)
+        return bloodGroups;
     } catch (error) {
         console.error(error);
     }
