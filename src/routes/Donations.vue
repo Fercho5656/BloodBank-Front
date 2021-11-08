fb2022dff5eb38c3f868179c2a7546751fd6e608
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-11-08 00:33:07 -0600

    Added method to fetch donation history

--- src/routes/Donations.vue
+++ src/routes/Donations.vue
@@ -2,18 +2,44 @@
   <div class="container text-white">
     <NewDonation />
     <hr />
-    <DonationHistory />
+    <DonationHistory :donations="donations" />
   </div>
+  <Loading v-if="isLoading" />
 </template>
 
 <script>
 import NewDonation from "../components/NewDonation.vue";
 import DonationHistory from "../components/DonationHistory.vue";
+import Loading from "../components/Loading.vue";
+import { getDonations } from "../services/API/Donations";
+import { onMounted, ref } from "vue";
 export default {
   name: "Donations",
   components: {
     NewDonation,
     DonationHistory,
+    Loading,
+  },
+  setup() {
+    const isLoading = ref(true);
+    const donations = ref([]);
+
+    const getDonationsList = async () => {
+      const donations = await getDonations();
+      console.table(donations);
+      return donations;
+    };
+
+    onMounted(async () => {
+      isLoading.value = true;
+      donations.value = await getDonationsList();
+      isLoading.value = false;
+    });
+
+    return {
+      donations,
+      isLoading
+    };
   },
 };
 </script>
