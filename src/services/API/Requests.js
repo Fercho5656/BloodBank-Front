fd346794cfd57beedb494bd527bd363cbd008478
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-11-08 00:27:52 -0600

    Added local date string to fetched date

--- src/services/API/Requests.js
+++ src/services/API/Requests.js
@@ -3,8 +3,14 @@ const ENDPOINT = 'https://localhost:5001/api/request/'
 export const getRequests = async () => {
     try {
         const response = await fetch(ENDPOINT)
-        const data = await response.json()
-        return data.$values
+        const { $values } = await response.json()
+        const requests = $values.map(request => {
+            return {
+                ...request,
+                date: new Date(request.date).toLocaleDateString()
+            }
+        })
+        return requests
     } catch (error) {
         console.error(error)
     }
@@ -14,6 +20,23 @@ export const createRequest = async (request) => {
     try {
         const response = await fetch(ENDPOINT, {
             method: 'POST',
+            headers: {
+                Application: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(request)
+        })
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const updateRequest = async (request, requestId) => {
+    try {
+        const response = await fetch(`${ENDPOINT}${requestId}`, {
+            method: 'PUT',
             headers: {
                 Application: 'application/json',
                 'Content-Type': 'application/json',
