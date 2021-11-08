0d2e0b13c90b7cdd5b36ff9e76658ec1a5c61667
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-11-08 00:27:32 -0600

    Corrected name (what a shame)

---
+++ src/components/Requests/PendingRequests.vue
@@ -0,0 +1,64 @@
+<template>
+  <h2 class="text-white">Peticiones Pendientes</h2>
+  <table class="table table-dark table-hover">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Fecha</th>
+        <th>Hospital</th>
+        <th>Grupo Sanguíneo</th>
+        <th>Cantidad (mL)</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(request, index) in requests" :key="request.$id">
+        <td>{{ index + 1 }}</td>
+        <td>{{ request.date }}</td>
+        <td>{{ request.hospital.name }}</td>
+        <td>{{ `${request.bloodGroup.bloodType}${request.bloodGroup.rh}` }}</td>
+        <td>{{ request.quantity }}</td>
+        <td>
+          <button
+            class="btn btn-success"
+            @click="$emit('accept-request', {request: request, status: 'Aceptada'})"
+          >
+            Aceptar
+          </button>
+          <button
+            class="btn btn-primary"
+            @click="$emit('reject-request', {request: request, status: 'Rechazada'})"
+          >
+            Rechazar
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "PendingRequests",
+  components: {},
+  props: {
+    requests: {
+      type: Array,
+      required: true,
+    },
+  },
+  emits: ["accept-request", "reject-request"],
+};
+</script>
+
+<style scoped>
+.buttons {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+}
+.btn {
+  margin: 5px;
+  flex: 1 0 25%;
+}
+</style>
