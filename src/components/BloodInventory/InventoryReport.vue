e89c619f41ba8099200a0a1801f45c606a58f01c
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-11-20 20:33:34 -0600

    Component for report

---
+++ src/components/BloodInventory/InventoryReport.vue
@@ -0,0 +1,20 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  name: "InventoryReport",
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+    title: String
+  },
+  setup() {},
+};
+</script>
+
+<style scoped>
+</style>
--- src/routes/BloodInventory.vue
+++ src/routes/BloodInventory.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="container">
-    <select
-      v-model="selectedBloodBank"
-      @change="changeBloodBank"
-      class="form-control"
-    >
-      <option value="0" disabled>Seleccione un banco</option>
-      <option
-        v-for="bloodBank in bloodBanks"
-        :value="bloodBank.id"
-        :key="bloodBank.id"
+    <header class="inventory-header">
+      <select
+        v-model="selectedBloodBank"
+        @change="changeBloodBank"
+        class="form-control mx-2"
       >
-        {{ bloodBank.bankName }}
-      </option>
-    </select>
+        <option value="0" disabled>Seleccione un banco</option>
+        <option
+          v-for="bloodBank in bloodBanks"
+          :value="bloodBank.id"
+          :key="bloodBank.id"
+        >
+          {{ bloodBank.bankName }}
+        </option>
+      </select>
+      <button class="btn btn-success" @click="prepareReport">
+        <i class="bi bi-download"></i>
+      </button>
+    </header>
     <div class="row">
       <div
         class="col-md-3"
@@ -28,10 +33,20 @@
     </div>
   </div>
   <Loading text="Cargando..." v-if="isLoading" />
+  <Modal
+    :show="showPrintModal"
+    @close="showPrintModal = false"
+    @keydown="showPrintModal = false"
+    tabindex="0"
+  >
+    <InventoryReport :data="reportData" :title="selectedBankName" />
+  </Modal>
 </template>
 
 <script>
 import BloodInfo from "../components/BloodInventory/BloodInfo.vue";
+import InventoryReport from "../components/BloodInventory/InventoryReport.vue";
+import Modal from "../components/Modal.vue";
 import Loading from "../components/Loading.vue";
 import { getByBankId } from "../services/API/BloodGroups";
 import { getAllBloodBanks } from "../services/API/BloodBanks";
@@ -41,12 +56,17 @@ export default {
   components: {
     BloodInfo,
     Loading,
+    Modal,
+    InventoryReport,
   },
   setup() {
     const bloodGroups = ref([]);
     const bloodBanks = ref([]);
     const selectedBloodBank = ref(3);
     const isLoading = ref(true);
+    const showPrintModal = ref(false);
+    const reportData = ref([]);
+    const selectedBankName = ref("");
 
     const getBloodGroups = async (id) => await getByBankId(id);
 
@@ -56,6 +76,17 @@ export default {
       isLoading.value = true;
       bloodGroups.value = await getBloodGroups(selectedBloodBank.value);
       isLoading.value = false;
+    };
+
+    const prepareReport = () => {
+      reportData.value = bloodGroups.value.map((bloodGroupInfo) => {
+        const { bloodGroup, quantity } = bloodGroupInfo;
+        return [`${bloodGroup.bloodType}${bloodGroup.rh}`, quantity];
+      });
+      selectedBankName.value = bloodBanks.value.filter(
+        (bloodBank) => bloodBank.id === selectedBloodBank.value
+      )[0].bankName;
+      showPrintModal.value = true;
     };
 
     onMounted(async () => {
@@ -75,10 +106,23 @@ export default {
       selectedBloodBank,
       isLoading,
       changeBloodBank,
+      showPrintModal,
+      prepareReport,
+      reportData,
+      selectedBankName
     };
   },
 };
 </script>
 
-<style>
+<style scoped>
+.inventory-header {
+  display: flex;
+  flex-flow: row nowrap;
+}
+@media print {
+  @page {
+    size: letter;
+  }
+}
 </style>
