e0f23c5e1e2062cf35c9a8cfa6a0efcfbe215fd0
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-11-22 14:52:05 -0600

    Component to show a piechart with the stock available

--- src/components/BloodInventory/InventoryReport.vue
+++ src/components/BloodInventory/InventoryReport.vue
@@ -1,20 +1,71 @@
 <template>
-<div></div>
+  <div class="report">
+    <header class="chart-header">
+      <h1 class="text-center mx-3">{{ title }}</h1>
+      <button class="btn btn-success print" @click="print">
+        <i class="bi bi-printer"></i>
+      </button>
+    </header>
+    <p class="text-center">{{ description }}</p>
+    <pie-chart
+      :data="data"
+      legend="bottom"
+      :bytes="true"
+      :library="library"
+      adapter="google"
+    ></pie-chart>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "InventoryReport",
   props: {
     data: {
       type: Array,
-      default: () => [],
+      required: true,
     },
-    title: String
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
   },
-  setup() {},
+  setup() {
+    const library = {
+      backgroundColor: "#fff",
+      pieSliceText: "value",
+      width: 500,
+      suffix: "ml",
+      legend: {
+        position: "bottom",
+        textStyle: { color: "#efef", fontSize: 16 },
+      },
+    };
+
+    const print = () => {
+      window.print();
+    };
+
+    return { library, print };
+  },
 };
 </script>
 
 <style scoped>
+.chart-header {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+}
+@media print {
+  .print {
+    display: none;
+  }
+  @page {
+    size: portrait;
+  }
+}
 </style>
